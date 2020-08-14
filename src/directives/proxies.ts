0fd462ad2e9e1e9b5fe80ae7ce62be3a5d7182dd
Elwin van Eede <elwinvaneede@gmail.com>
2020-08-14 19:27:05 +0200

    New build from the core package

--- src/directives/proxies.ts
+++ src/directives/proxies.ts
@@ -1,51 +1,21 @@
 /* tslint:disable */
 /* auto-generated angular directive proxies */
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, ElementRef, EventEmitter, NgZone } from '@angular/core';
-import { fromEvent } from 'rxjs';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, ElementRef, NgZone } from '@angular/core';
+import { ProxyCmp, proxyOutputs } from './angular-component-lib/utils';
 
-export const proxyInputs = (Cmp: any, inputs: string[]) => {
-  const Prototype = Cmp.prototype;
-  inputs.forEach(item => {
-    Object.defineProperty(Prototype, item, {
-      get() { return this.el[item]; },
-      set(val: any) { this.z.runOutsideAngular(() => (this.el[item] = val)); }
-    });
-  });
-};
+import { Components } from 'elwins-test-web-components';
 
-export const proxyMethods = (Cmp: any, methods: string[]) => {
-  const Prototype = Cmp.prototype;
-  methods.forEach(methodName => {
-    Prototype[methodName] = function () {
-      const args = arguments;
-      return this.z.runOutsideAngular(() => this.el[methodName].apply(this.el, args));
-    };
-  });
-};
-
-export const proxyOutputs = (instance: any, el: any, events: string[]) => {
-  events.forEach(eventName => instance[eventName] = fromEvent(el, eventName));
-}
-
-// tslint:disable-next-line: only-arrow-functions
-export function ProxyCmp(opts: { inputs?: any; methods?: any }) {
-  const decorator =  function(cls: any){
-    if (opts.inputs) {
-      proxyInputs(cls, opts.inputs);
-    }
-    if (opts.methods) {
-      proxyMethods(cls, opts.methods);
-    }
-    return cls;
-  };
-  return decorator;
-}
-
-import { Components } from 'elwins-test-web-components'
 
 export declare interface EveButton extends Components.EveButton {}
-@ProxyCmp({inputs: ['fill', 'href', 'rel', 'target']})
-@Component({ selector: 'eve-button', changeDetection: ChangeDetectionStrategy.OnPush, template: '<ng-content></ng-content>', inputs: ['fill', 'href', 'rel', 'target'] })
+@ProxyCmp({
+  inputs: ['fill', 'href', 'rel', 'target']
+})
+@Component({
+  selector: 'eve-button',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  template: '<ng-content></ng-content>',
+  inputs: ['fill', 'href', 'rel', 'target']
+})
 export class EveButton {
   protected el: HTMLElement;
   constructor(c: ChangeDetectorRef, r: ElementRef, protected z: NgZone) {
@@ -54,9 +24,17 @@ export class EveButton {
   }
 }
 
+
 export declare interface EveIntroduction extends Components.EveIntroduction {}
-@ProxyCmp({inputs: ['first', 'last', 'middle']})
-@Component({ selector: 'eve-introduction', changeDetection: ChangeDetectionStrategy.OnPush, template: '<ng-content></ng-content>', inputs: ['first', 'last', 'middle'] })
+@ProxyCmp({
+  inputs: ['first', 'last', 'middle']
+})
+@Component({
+  selector: 'eve-introduction',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  template: '<ng-content></ng-content>',
+  inputs: ['first', 'last', 'middle']
+})
 export class EveIntroduction {
   protected el: HTMLElement;
   constructor(c: ChangeDetectorRef, r: ElementRef, protected z: NgZone) {
